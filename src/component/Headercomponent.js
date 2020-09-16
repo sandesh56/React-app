d0b181f7c9cb2442593843d476e4216df460fdaf
sandesh <sandeshrauth644@gmail.com>
2020-09-16 23:29:25 +0545

    uncontrolled-form

--- src/component/Headercomponent.js
+++ src/component/Headercomponent.js
@@ -1,27 +1,35 @@
 import React, { Component } from "react";
 import {
-    Navbar,
-    NavbarBrand,
-    Jumbotron,
-    NavItem,
-    Nav,
-    NavbarToggler,
-    Collapse,
-} from "reactstrap";
+    Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+    Button, Modal, ModalHeader, ModalBody,
+    Form, FormGroup, Input, Label
+} from 'reactstrap';
 import { NavLink } from "react-router-dom";
 
 export class Headercomponent extends Component {
     constructor(props) {
         super(props);
-        this.toggleNav = this.toggleNav.bind(this);
         this.state = {
             isNavopen: false,
+            ismodalopen: false
         };
+        this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handelchange = this.handelchange.bind(this);
     }
 
     toggleNav() {
         this.setState({ isNavopen: !this.state.isNavopen });
     }
+    toggleModal() {
+        this.setState({ ismodalopen: !this.state.ismodalopen });
+    }
+    handelchange(event) {
+        this.toggleModal();
+        alert("your username is " + this.username.value + " and password is " + this.password.value + " remember me : " + this.remember.checked);
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div>
@@ -44,7 +52,15 @@ export class Headercomponent extends Component {
                                     <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span>  Contact Us</NavLink>
                                 </NavItem>
                             </Nav>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal}>
+                                        <span className="fa fa-sign fa-lg">Sign in</span>
+                                    </Button>
+                                </NavItem>
+                            </Nav>
                         </Collapse>
+
                     </div>
                 </Navbar>
                 <Jumbotron>
@@ -61,6 +77,35 @@ export class Headercomponent extends Component {
                         </div>
                     </div>
                 </Jumbotron>
+                <Modal isOpen={this.state.ismodalopen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>
+                        Login
+                        </ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handelchange}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="text" id="password" name="password"
+                                    innerRef={(input) => this.password = input} />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                        innerRef={(input) => this.remember = input} />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <FormGroup className="mt-2">
+                                <Button type="submit" value="submit" color="primary">Login</Button>
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+                </Modal>
             </div>
         );
     }
